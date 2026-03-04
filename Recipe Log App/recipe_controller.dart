be0b392edb67f923b8ae3e70f29da350d96518c3
Rashed Khan <118573694+RashedKhanSezan@users.noreply.github.com>
2026-03-05 01:43:58 +0600

    Implement RecipeController to fetch recipes

---
+++ Recipe Log App/recipe_controller.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:recipe_log/model/recipe_model.dart';
+
+class RecipeController extends GetxController {
+  var recipes = <RecipeModel>[].obs;
+  var isLoading = true.obs;
+
+  void fetchRecipe() async {
+    try {
+      isLoading(true);
+      var response = await http.get(Uri.parse('https://dummyjson.com/recipes'));
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+
+        var list = (data['recipe'] as List)
+            .map((item) => RecipeModel.fromJson(item))
+            .toList();
+        recipes.assignAll(list);
+      }
+    } catch (e) {
+      Get.snackbar('Error', 'Failed to load');
+    } finally {
+      isLoading(false);
+    }
+  }
+}
