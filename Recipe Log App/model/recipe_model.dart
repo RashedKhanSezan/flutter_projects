6ce8a6669e475f1c2787b0d16b9666d29035f1fe
Rashed Khan <118573694+RashedKhanSezan@users.noreply.github.com>
2026-03-10 19:35:35 +0600

    Add cook time to RecipeModel
    Added 'time' property to RecipeModel and updated JSON parsing.
    fixed type errors

--- Recipe Log App/model/recipe_model.dart
+++ Recipe Log App/model/recipe_model.dart
@@ -5,23 +5,26 @@ class RecipeModel {
   final String image;
   final double rating;
   final String cuisine;
+  final double time;
 
-  RecipeModel({
-    required this.id,
-    required this.name,
-    required this.ingredients,
-    required this.image,
-    required this.rating,
-    required this.cuisine,
-  });
+  RecipeModel(
+      {required this.id,
+      required this.name,
+      required this.ingredients,
+      required this.image,
+      required this.rating,
+      required this.cuisine,
+      required this.time});
 
   factory RecipeModel.fromJson(Map<String, dynamic> json) {
     return RecipeModel(
-        id: json["id"],
-        name: json["name"],
-        ingredients: List.from((json["ingredients"])),
-        image: json["image"],
-        rating: json["rating"] as double,
-        cuisine: json["cuisine"]);
+      id: json["id"],
+      name: json["name"],
+      ingredients: List.from((json["ingredients"])),
+      image: json["image"],
+      rating: (json["rating"] as num).toDouble(),
+      cuisine: json["cuisine"],
+      time: (json['cookTimeMinutes'] as num).toDouble(),
+    );
   }
 }
