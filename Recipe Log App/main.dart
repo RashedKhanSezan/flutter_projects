ac43fb6cb8e63d1ed6a85e6405e3aff660737c37
Rashed Khan <118573694+RashedKhanSezan@users.noreply.github.com>
2026-03-10 19:33:02 +0600

    Refactor main function to use RecipeLog widget

--- Recipe Log App/main.dart
+++ Recipe Log App/main.dart
@@ -1,6 +1,17 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:recipe_log/views/recipe.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(const RecipeLog());
+
+class RecipeLog extends StatelessWidget {
+  const RecipeLog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: const RecipeScreen(),
+    );
+  }
 }
